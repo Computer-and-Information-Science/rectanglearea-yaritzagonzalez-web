95e88c853da5c24da4fe0bd536704a3e49418082
Yari G <jebbygonzalez@gmail.com>
2025-09-10 21:29:43 +0000

    Calculates area and perimeter

--- main.cpp
+++ main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
+  
+  double area;
+  double perimeter;
 
+  //calculations
+  area = length * width;
+  perimeter = 2 * length + 2 * width;
+
+  cout << "Rectangle Properties:" << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
 
 }
